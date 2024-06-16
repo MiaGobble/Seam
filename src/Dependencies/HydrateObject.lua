ea913dd7374e77a96f3bf87ef66524594aeaaca3
Iconic Gaming <96698128+MiaGobble@users.noreply.github.com>
2024-06-16 12:14:52 -0400

    Create HydrateObject.lua

---
+++ src/Dependencies/HydrateObject.lua
@@ -0,0 +1,32 @@
+-- Author: iGottic
+
+--[=[
+    @class HydrateObject
+    @ignore
+]=]
+
+local HydrateObject = {}
+
+function HydrateObject:__call(Object : Instance, Properties : {[any] : any})
+    for Index, Property in Properties do
+        if typeof(Index) == "table" then
+             if Index.__SPHI_INDEX then
+                Index(Object, Property)
+             else
+                error(`Object hydration recieved invalid index type! Expected Sphi object or string, got table ({Object:GetFullName()})`)
+             end
+        elseif typeof(Index) == "table" then
+            if Property.__SPHI_OBJECT then
+                Property(Object, Index)
+            else
+                error("Invalid property type! Expected Sphi object or string, got table")
+            end
+        else
+            Object[Index] = Property
+        end
+    end
+
+    return Object
+end
+
+return setmetatable({}, HydrateObject)
