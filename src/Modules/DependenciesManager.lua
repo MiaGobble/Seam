25ebb698bafaa55f1f9010068773cef4ea9d914a
Iconic Gaming <96698128+MiaGobble@users.noreply.github.com>
2024-07-28 22:31:40 -0400

    SPHI-5 Initialized prototype for dependencies

--- src/Modules/DependenciesManager.lua
+++ src/Modules/DependenciesManager.lua
@@ -1,1 +1,21 @@
--- TODO: DependenciesManager.lua
+local DependenciesManager = {}
+
+local function GetObjectType(Object : any) : string
+    if typeof(Object) == "Instance" then
+        return "Instance"
+    elseif typeof(Object) == "table" then
+        if Object.__SPHI_OBJECT then
+            return "SphiObject"
+        end
+    end
+
+    return "unknown"
+end
+
+function DependenciesManager:AttachStateToObject(Object : any, StateInstance : any)
+    local ObjectType = GetObjectType(Object)
+
+    
+end
+
+return DependenciesManager
